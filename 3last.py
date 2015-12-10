4015ce4d469974d6e3be41677eb4971005cdc3fa
czaonline <czaonline123@gmail.com>
2015-12-11 00:29:19 +0700

    fix 3last num

--- 3last.py
+++ 3last.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 def make_input(prize):
     import csv
     with open("List_of_lotto_2010_2014 - Sheet1.csv") as csvfile:
@@ -20,14 +21,68 @@ def make_input(prize):
         elif prize == '3num4':
             prize_n = [row['3Last1'] for row in reader]
             return prize_n
-def make_input():
-        prize1 = csv_reader()
-        prize2 = [row['3Last2'] for row in reader]
-        #prize3 = [row['3Last3'] for row in reader]
-        #prize4 = [row['3Last4'] for row in reader]
-        print(prize1)
-        #print(prize2)
-        #print(prize3)
-        #print(prize4)
-make_input()
-csv_reader()
+            
+def compile_number(year, table, dic):
+    if year == "2010":
+        count = 24
+        for i in range(count):
+            #this 2Last_lotto in 2010      
+            if table[i] not in dic:
+                dic[table[i]] = 1
+            else:
+                dic[table[i]] += 1
+        return dic
+                
+    elif year == "2011":
+        count = 48
+        for i in range(24, count):
+            #this 2Last_lotto in 2011
+            if table[i] not in dic:
+                dic[table[i]] = 1
+            else:
+                dic[table[i]] += 1
+        return dic
+                
+    elif year == "2012":
+        count = 72
+        for i in range(48, count):
+            #this 2Last_lotto in 2012
+            if table[i] not in dic:
+                dic[table[i]] = 1
+            else:
+                dic[table[i]] += 1
+        return dic
+                
+    elif year == "2013":
+        count = 96
+        for i in range(72, count):
+            #this 2Last_lotto in 2013
+            if table[i] not in dic:
+                dic[table[i]] = 1
+            else:
+                dic[table[i]] += 1
+        return dic
+                
+    elif year == "2014":
+        count = 120
+        for i in range(96, count):
+            #this 2Last_lotto in 2014
+            if table[i] not in dic:
+                dic[table[i]] = 1
+            else:
+                dic[table[i]] += 1
+        return dic
+    
+def lotto_two():
+        dic_two_2010 = compile_number("2010", make_input("2num"), dict())
+        dic_two_2011 = compile_number("2011", make_input("2num"), dict())
+        dic_two_2012 = compile_number("2012", make_input("2num"), dict())
+        dic_two_2013 = compile_number("2013", make_input("2num"), dict())
+        dic_two_2014 = compile_number("2014", make_input("2num"), dict())
+        print(dic_two_2010)
+        
+        
+
+lotto_two()
+
+##ตัวแปรที่ต้องแก้มี dic_two, table, i
--- 3last2.py
+++ 3last2.py
@@ -1,85 +1,55 @@
-import csv
-with open("List_of_lotto_2010_2014 - Sheet1 (1).csv") as csvfile:
-    reader = csv.DictReader(csvfile)
-    
-    prize = [row['3Last2'] for row in reader]
-    year_10 = {}
-    year_11 = {}
-    year_12 = {}
-    year_13 = {}
-    year_14 = {}
+def make_num(prize, dic):
     count = 24
     for num in range(count):
         if prize[num] not in year_10:
-            year_10[prize[num]] = 1
+            dic[prize[num]] = 1
         else: 
-            year_10[prize[num]] += 1
+            dic[prize[num]] += 1
     count = 48
     for num in range(24, count):
         if prize[num] not in year_11:
-            year_11[prize[num]] = 1
+            dic[prize[num]] = 1
         else: 
-            year_11[prize[num]] += 1
+            dic[prize[num]] += 1
             
     count = 72
     for num in range(48,count):
         if prize[num] not in year_12:
-            year_12[prize[num]] = 1
+            dic[prize[num]] = 1
         else: 
-            year_12[prize[num]] += 1
+            dic[prize[num]] += 1
             
     count = 96
     for num in range(72, count):
         if prize[num] not in year_13:
-            year_13[prize[num]] = 1
+            dic[prize[num]] = 1
         else: 
-            year_13[prize[num]] += 1
+            dic[prize[num]] += 1
             
     count = 120
     for num in range(96, count):
         if prize[num] not in year_14:
-            year_14[prize[num]] = 1
+            dic[prize[num]] = 1
         else: 
-            year_14[prize[num]] += 1
-    
-    print(year_10)
-    print(year_11)
-    print(year_12)
-    print(year_13)
-    print(year_14)
-    
+            dic[prize[num]] += 1
+
 def make_3():
-    count = 24
-        for num in range(count):
-        if prize[num] not in year_10:
-            year_10[prize[num]] = 1
-        else: 
-            year_10[prize[num]] += 1
-    count = 48
-    for num in range(24, count):
-        if prize[num] not in year_11:
-            year_11[prize[num]] = 1
-        else: 
-            year_11[prize[num]] += 1
-            
-    count = 72
-    for num in range(48,count):
-        if prize[num] not in year_12:
-            year_12[prize[num]] = 1
-        else: 
-            year_12[prize[num]] += 1
-            
-    count = 96
-    for num in range(72, count):
-        if prize[num] not in year_13:
-            year_13[prize[num]] = 1
-        else: 
-            year_13[prize[num]] += 1
-            
-    count = 120
-    for num in range(96, count):
-        if prize[num] not in year_14:
-            year_14[prize[num]] = 1
-        else: 
-            year_14[prize[num]] += 1
-def 
+    import csv
+    with open("List_of_lotto_2010_2014 - Sheet1.csv") as csvfile:
+        reader = csv.DictReader(csvfile)
+        prize = [row['3Last2'] for row in reader]
+    for i in range(1, 4):
+        if i == '1':
+            year_10 = make_num(prize, dict())
+            year_11 = make_num(prize, dict())
+            year_12 = make_num(prize, dict())
+            year_13 = make_num(prize, dict())
+            year_14 = make_num(prize, dict())
+        elif i == '2':
+            year_10 = make_num(prize, year_10)
+            year_11 = make_num(prize, year_11)
+            year_12 = make_num(prize, year_12)
+            year_13 = make_num(prize, year_13)
+            year_14 = make_num(prize, year_14)
+    print(year_10)
+make_3()
